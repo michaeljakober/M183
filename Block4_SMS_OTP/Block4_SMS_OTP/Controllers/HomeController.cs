2099bf95cb2f01068e6362403443460fdbe853e6
michaeljakober <michael_jakober@bluewin.ch>
2018-09-10 14:37:43 +0200

    Hoi

---
+++ Block4_SMS_OTP/Block4_SMS_OTP/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Block4_SMS_OTP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login()
+        {
+            var username = Request["username"];
+            var password = Request["password"];
+
+            if (username == "test" && password == "test")
+            {
+                var request = (HttpWebRequest) WebRequest.Create("https://rest.nexmo.com/sms/json");
+
+                var secret = "TEST_SECRET";
+
+                var postData = "api_key";
+            }
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
