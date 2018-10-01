7be9a8bceefbd51ca7e43e555608b1e9d45bda6c
michaeljakober <michael_jakober@bluewin.ch>
2018-10-01 14:22:44 +0200

    Finished SSO

--- Block6_SSO/Block6_SSO/Controllers/HomeController.cs
+++ Block6_SSO/Block6_SSO/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Text;
 using System.Web;
 using System.Web.Mvc;
 
@@ -25,6 +28,26 @@ namespace Block6_SSO.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+
+        public JsonResult SSOTokenSignin()
+        {
+            var idToken = Request["idtoken"];
+            var request = (HttpWebRequest)WebRequest.Create("https://www.googleapis.com/oauth2/v3/tokeninfo?id_token=" + idToken);
+            var postData = "id_token=" + idToken;
+            var data = Encoding.ASCII.GetBytes(postData);
+            request.Method = "POST";
+            request.ContentType = "application/x-www-form-urlencoded";
+            request.ContentLength = data.Length;
+            using (var stream = request.GetRequestStream())
+            {
+                stream.Write(data, 0, data.Length);
+            }
+            var response = (HttpWebResponse)request.GetResponse();
+            var responseString = new StreamReader(response.GetResponseStream()).ReadToEnd();
+
+            return Json(responseString);
         }
     }
 }
