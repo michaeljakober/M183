1ede9885e120b46d8efda312cf6d0674034d53e0
michaeljakober <michael_jakober@bluewin.ch>
2018-11-26 13:41:08 +0100

    Initial Commit for Block 12

---
+++ Block12_SQLInjection/Block12_SQLInjection/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Block12_SQLInjection
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Block12_SQLInjection/Block12_SQLInjection/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Block12_SQLInjection.MvcApplication" Language="C#" %>
